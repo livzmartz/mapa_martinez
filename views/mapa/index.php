3ba3050d9a9570d51e9a0d046a3ef3082fee0962
Livni <llmc0383@gmail.com>
2023-09-07 23:34:59 -0600

    se agrego la vista y correccion de layout

---
+++ views/mapa/index.php
@@ -0,0 +1,18 @@
+<h1 class="text-center">MAPA DE GUATEMALA</h1>
+
+<div class="row justify-content-center">
+    <div class="col-lg-1 d-grid mb-lg-0 mb-2 mx-auto text-center">
+    <br>  
+        <button class="btn btn-danger btn-sm" id="actualizar" name="actualizar">ACTUALIZAR</button>
+    </div>
+</div>
+<br>
+<div class="row justify-content-center">
+    <div class="col-lg-11 border rounded" id="mapa" style="height: 60vh; min-height:auto"></div>
+</div>
+
+<script src="<?= asset('./build/js/mapa/index.js') ?>"></script>
+
+
+
+
